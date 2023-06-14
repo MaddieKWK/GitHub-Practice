e2cb2190b7b0460c601959b1999afee01be98a6b
Madaline Webb <madalinewebb@icloud.com>
2023-06-14 13:39:24 -0500

    first commit

--- GitHub Practice/ContentView.swift
+++ GitHub Practice/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(
+                red: 255/255, green: 204/255, blue: 229/255).ignoresSafeArea()
+            VStack (alignment: .leading, spacing: 20.0) {
+                
+            Text("Welcome to Study Central!")
+                .font(.title)
+                .foregroundColor(Color(hue: 0.287, saturation: 0.298, brightness: 0.817))
+                .multilineTextAlignment(.center)
+                
+                Image("book")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100.0, height: 100.0)
+                    .cornerRadius(15)
+
+            Text("The fun, safe place for studying")
+                    .font(.headline)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                                }
+                
+                
+        }
     }
 }
 
