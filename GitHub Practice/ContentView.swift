71c160cad99f13da6181d67bcd81a6a3c303d5ba
Renee Li <reneelee127@gmail.com>
2023-06-14 11:59:22 -0700

    Second Commit

--- GitHub Practice/ContentView.swift
+++ GitHub Practice/ContentView.swift
@@ -11,32 +11,33 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color(
-                red: 255/255, green: 204/255, blue: 229/255).ignoresSafeArea()
+                red: 242/255, green: 201/255, blue: 213/255).ignoresSafeArea()
             VStack (alignment: .leading, spacing: 20.0) {
-                
-            Text("Welcome to Study Central!")
-                .font(.title)
-                .foregroundColor(Color(hue: 0.287, saturation: 0.298, brightness: 0.817))
-                .multilineTextAlignment(.center)
-                
-                Image("book")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100.0, height: 100.0)
-                    .cornerRadius(15)
-
-            Text("The fun, safe place for studying")
-                    .font(.headline)
-                    .foregroundColor(Color.white)
+                Text("Welcome to Study Central!   ")
+                    .font(.custom("American Typewriter", size: 45))
+                    .foregroundColor(Color(hue: 0.287, saturation: 0.298, brightness: 0.817))
                     .multilineTextAlignment(.center)
-                
-                                }
-                
-                
+              
+                HStack(alignment: .center) {
+                    
+                    
+                    Image("book")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100.0, height: 100.0, alignment: .center)
+                        .cornerRadius(15)
+                    
+                    Text("The fun, safe place for studying")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                    
+                }
+            }
+
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
