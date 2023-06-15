181c8ccc5f90c2b5b3d50a35a155e722dd5ae0ce
Madaline Webb <madalinewebb@icloud.com>
2023-06-15 11:56:41 -0500

    Sixth commit

--- GitHub Practice/Quotes.swift
+++ GitHub Practice/Quotes.swift
@@ -9,10 +9,40 @@ import SwiftUI
 
 struct Quotes: View {
     var body: some View {
-        Text("Put some good quotes here.")
+        ZStack {
+            GeometryReader { geo in
+            Image("Wallpaper")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            }
+        VStack {
+            Text("Motivational quotes!")
+                .font(.custom("Amatic SC Bold", size: 45))
+                
+            Text("If you are having trouble focusing, or just need some motivation, this is a great place to be!")
+                .foregroundColor(Color.white)
+                .multilineTextAlignment(.center)
+                .font(.custom("Amatic SC Bold", size: 28))
+    
+            
+            Text("''Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do.'' - Pelé, Brazillian pro footballer")
+                .font(.custom("Amatic SC Bold", size: 23))                .foregroundColor(Color(hue: 0.892, saturation: 0.57, brightness: 0.794))
+                .padding(10.0)
+            
+            
+            
+            
+            
+
+            
+            
+        }
+
+      
     }
 }
-
+}
 struct Quotes_Previews: PreviewProvider {
     static var previews: some View {
         Quotes()
