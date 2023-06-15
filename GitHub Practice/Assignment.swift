61bcd099688536c9a8ca7d20327254e37c9e4eb0
Pranati Arni <pranatiarni@gmail.com>
2023-06-14 19:50:29 -0500

    Just added a few things to the Assignment page

--- GitHub Practice/Assignment.swift
+++ GitHub Practice/Assignment.swift
@@ -8,13 +8,91 @@
 import SwiftUI
 
 struct Assignment: View {
+    @State private var name = "Enter Here"
+    @State private var min = "Enter in minutes"
+    @State private var time = "Enter Here"
+    @State private var stop = "Enter like this: 25 min work - 5 min break"
+    @State private var textTitle = "Assignment Name"
     var body: some View {
-        Text("Assignment Details Page")
+            ZStack {
+                Image("background")
+                VStack {
+                    
+                    
+                    Text("Assignment Details")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .padding()
+                    
+                    Text(textTitle)
+                    
+                    
+                    TextField("", text: $name)
+                        .multilineTextAlignment(.center)
+                        .font(.title2)
+                        .border(Color.black, width: 1)
+                        .padding()
+                        
+                    
+                    Text("Time it takes to complete assigment")
+                        .font(.headline)
+                        .foregroundColor(Color(hue: 0.395, saturation: 1.0, brightness: 0.017))
+                    
+                    TextField("", text: $min)
+                        .multilineTextAlignment(.center)
+                        .font(.title2)
+                        .border(Color.black, width: 1)
+                        .padding()
+                        
+                    Text("Complete assignment by-")
+                        .font(.headline)
+                    
+                    TextField("", text: $time)
+                        .multilineTextAlignment(.center)
+                        .font(.title2)
+                        .border(Color.black, width: 1)
+                        .padding()
+                        
+                    
+                    Text("Break Times")
+                        .font(.headline)
+                    
+                    TextField("", text: $stop)
+                        .multilineTextAlignment(.center)
+                        .font(.title2)
+                        .border(Color.black, width: 1)
+                        .padding()
+                       
+                    
+//                    NavigationLink(destination: assignmentdetails2()) {
+                        Text("Submit")
+                            .font(.title)
+                            .padding()
+                            .frame(width: 150.0, height: 45.0)
+                            .background(.black)
+                            .foregroundColor(.white)
+                            .cornerRadius(16)
+                        
+                        
+                            .padding()
+                    }
+                }
+                
+                
+            }
+        }
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    struct Assignment_Previews: PreviewProvider {
+        static var previews: some View {
+            Assignment()
+        }
     }
-}
 
-struct Assignment_Previews: PreviewProvider {
-    static var previews: some View {
-        Assignment()
-    }
-}
