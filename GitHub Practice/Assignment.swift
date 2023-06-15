af688be351714fd21c1115f61f1ab54dd24e5945
Pranati Arni <pranatiarni@gmail.com>
2023-06-15 13:59:15 -0500

    commit

--- GitHub Practice/Assignment.swift
+++ GitHub Practice/Assignment.swift
@@ -8,90 +8,102 @@
 import SwiftUI
 
 struct Assignment: View {
-    @State private var name = "Enter Here"
-    @State private var min = "Enter in minutes"
-    @State private var time = "Enter Here"
-    @State private var stop = "Enter like this: 25 min work - 5 min break"
-    @State private var textTitle = "Assignment Name"
-    var body: some View {
-        NavigationView {
-            
-            
-            ZStack {
-                Image("background")
-                VStack {
-                    HStack {
-                        NavigationLink(destination: ContentView()) {Text("Back") }
+        @State private var name = "Enter Here"
+        @State private var min = "Enter in minutes"
+        @State private var time = "Enter Here"
+        @State private var stop = "Enter like this: 25 min work - 5 min break"
+        @State private var textTitle = "Assignment Name"
+        @State private var timeTitle = "Time it takes to complete assignment"
+        @State private var completeTitle = "Complete assignment by"
+        @State private var breakTitle = "Break Times"
+        
+        var body: some View {
+    //        NavigationStack {
+                ZStack {
+                    Image("background")
+                    VStack {
+                        
                         
                         Text("Assignment Details")
-                            .font(.custom("Amatic SC Bold", size: 45))
+                            .font(.title)
                             .fontWeight(.heavy)
+                            .foregroundColor(Color(hue: 0.284, saturation: 0.291, brightness: 0.763))
                             .padding()
+                        
+                        Text(textTitle)
+                            
+                        
+                        TextField("", text: $name)
+                            .multilineTextAlignment(.center)
+                            .font(.title2)
+                            .border(Color.black, width: 1)
+                            .padding()
+                            
+                        
+                        Text(timeTitle)
+                            .font(.headline)
+                            .foregroundColor(Color(hue: 0.395, saturation: 1.0, brightness: 0.017))
+                        
+                        TextField("", text: $min)
+                            .multilineTextAlignment(.center)
+                            .font(.title2)
+                            .border(Color.black, width: 1)
+                            .padding()
+                           
+                        Text(completeTitle)
+                            .font(.headline)
+                        
+                        TextField("", text: $time)
+                            .multilineTextAlignment(.center)
+                            .font(.title2)
+                            .border(Color.black, width: 1)
+                            .padding()
+                           
+                        
+                        Text(breakTitle)
+                            .font(.headline)
+                        
+                        TextField("", text: $stop)
+                            .multilineTextAlignment(.center)
+                            .font(.title2)
+                            .border(Color.black, width: 1)
+                            .padding()
+                           
+                        
+                        
+                        Button("Submit") {
+                            textTitle = "\(name)"
+                            timeTitle = "Try to complete assignment in \(min)"
+                            completeTitle = "by \(time)"
+                            breakTitle = "Takes breaks often! Here is your break scuedule: \(stop)"
+                        }
+
+                        .font(.title2)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.black)
+                        
+                        
+                        
+                    
                     }
-                   
+    //
+                                
                     
-                    Text(textTitle)
-                    
-                    
-                    TextField("", text: $name)
-                        .multilineTextAlignment(.center)
-                        .font(.title2)
-                        .border(Color.black, width: 1)
-                        .padding()
+                .padding()
+                        }
+                    }
                     
                     
-                    Text("Time it takes to complete assigment")
-                        .font(.headline)
-                        .foregroundColor(Color(hue: 0.395, saturation: 1.0, brightness: 0.017))
-                    
-                    TextField("", text: $min)
-                        .multilineTextAlignment(.center)
-                        .font(.title2)
-                        .border(Color.black, width: 1)
-                        .padding()
-                    
-                    Text("Complete assignment by-")
-                        .font(.headline)
-                    
-                    TextField("", text: $time)
-                        .multilineTextAlignment(.center)
-                        .font(.title2)
-                        .border(Color.black, width: 1)
-                        .padding()
-                    
-                    
-                    Text("Break Times")
-                        .font(.headline)
-                    
-                    TextField("", text: $stop)
-                        .multilineTextAlignment(.center)
-                        .font(.title2)
-                        .border(Color.black, width: 1)
-                        .padding()
-                    
-                    
-                    Text("Submit")
-                        .font(.custom("Amatic SC Bold", size: 27))
-                        .padding()
-                        .frame(width: 150.0, height: 45.0)
-                        .background(.black)
-                        .foregroundColor(.white)
-                        .cornerRadius(16)
-                    
-                    
-                        .padding()
                 }
-                
-            }
-                
-            }
-        }
+            
+            
+            
+            
+            
+            
         
-    }
-    
-    
-    
-    
+        
+        
     
     
     
--- GitHub Practice/Timer.swift
+++ GitHub Practice/Timer.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct Timer: View {
+    @State var countDownTimer = 5
+    @State var timerRunning = false
+    let timer = Timer.publish(every: 1, on: .main, in: .common) .autoconnect()
     var body: some View {
-        Text("Timer Page")
+        
+        VStack {
+           Text("\(countDownTimer)")
+                .onReceive(timer) { _ in
+                    if countDownTimer > 0 && timerRunning {
+                        countDownTimer -= 1
+                    } else {
+                        timerRunning = false
+                     }
+                    }
+                .font(.system(size: 80, weight: .bold))
+                .opacity(0.80)
+            
+            HStack (spacing : 30) {
+                Button("Start") {
+                    timerRunning = true
+                        
+                }
+                
+                Button("Reset") {
+                    countDownTimer = 5
+                }
+            }
+        }
+       
     }
 }
 
